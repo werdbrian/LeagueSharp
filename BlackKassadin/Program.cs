5bbba6e985e5973face71ace15c5255aa15b3be2
werdbrian <werdbrian@gmail.com>
2015-04-16 14:34:34 -0700

    Update Program.cs

--- BlackKassadin/Program.cs
+++ BlackKassadin/Program.cs
@@ -60,7 +60,7 @@ namespace BlackKassadin
 
             _nullSphere = new Spell(SpellSlot.Q, 650f);
             _netherBlade = new Spell(SpellSlot.W, 150f);
-            _forcePulse = new Spell(SpellSlot.E, 400f);
+            _forcePulse = new Spell(SpellSlot.E, 700f);
             _riftWalk = new Spell(SpellSlot.R, 500f);
 
             SpellList.AddRange(new[] { _nullSphere, _forcePulse, _riftWalk });
